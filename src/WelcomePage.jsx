568245fdd433cd83afbd1a997ada79fa6c9a491b
Ariana V <arlexavega@gmail.com>
2026-02-05 21:38:59 -0500

    fighting for my life while localhost doesn't want to work

--- src/WelcomePage.jsx
+++ src/WelcomePage.jsx
@@ -1,3 +1,25 @@
 export default function WelcomePage() {
   
+  const {start, highScores} = useGame();
+
+  return (
+
+    <>
+    <div className="welcome">
+      <p>Welcome to Whack a Mole!</p>
+      <p>Whack a mole to earn points.</p>
+      <p>How many can you get?</p>
+      <button onClick={start}>Play</button>
+    </div>
+
+    <div className="highscore">
+      <h2>High Scores</h2>
+      <ul>
+        {highScores.length > 0
+        
+        }
+      </ul>
+    </div>
+    </>
+  )
 }
